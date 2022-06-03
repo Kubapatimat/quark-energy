ec9ab68a47b1e0a8bd84487a86d2dc9d419165ea
Kubapatimat <halfarkuba@gmail.com>
2022-06-03 12:44:44 +0200

    Added vector origin and fixed distance function

--- js/quark.js
+++ js/quark.js
@@ -9,7 +9,7 @@ class Quark {
 
     const quark_ball = new Konva.Circle({
       x: 0,
-      y: 0,
+      y: -R,
       radius: R,
       fill: color,
       name: "quark"
@@ -17,7 +17,7 @@ class Quark {
 
     const quark_arrow = new Konva.Arrow({
       x: 0,
-      y: 0,
+      y: -R,
       points: [0, R, 0, -R + 6],
       pointerLength: 16,
       pointerWidth: 12,
@@ -26,9 +26,18 @@ class Quark {
       strokeWidth: 4,
       name: "quark"
     });
+    
+    const quark_arrow_origin = new Konva.Circle({
+      x: 0,
+      y: 0,
+      radius: 5,
+      fill: "black",
+      name: "quark"
+    })
 
     this.group.add(quark_ball);
     this.group.add(quark_arrow);
+    this.group.add(quark_arrow_origin);
 
     this.group.on("transform dragmove", (e) => updateCallback(e, this.group));
     this.group.on("click tap", (e) => clickCallback(e, this.group));
